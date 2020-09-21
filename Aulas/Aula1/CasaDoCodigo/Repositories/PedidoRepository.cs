b21724d60dbe23afc58f935bd738d9bc36cc85af
Silvio Antônio de Oliveira Júnior <silvio.junior@crp.com>
2020-09-21 10:03:37 -0300

    Obtendo ou criando novo pedido

--- Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -7,13 +7,30 @@ using System.Threading.Tasks;
 
 namespace CasaDoCodigo.Repositories
 {
-    public interface IPedidoRepository { }
+    public interface IPedidoRepository
+    {
+        Pedido GetPedido();
+    }
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
     {
         private readonly IHttpContextAccessor contextAccessor;
         public PedidoRepository(Applicationcontext applicationcontext, IHttpContextAccessor contextAccessor) : base(applicationcontext)
         {
             this.contextAccessor = contextAccessor;
+        }
+
+        public Pedido GetPedido()
+        {
+            var pedidoId = GetPedidoId();
+            var pedido = dbSets.Where(p => p.Id == pedidoId).SingleOrDefault();
+
+            if (pedido == null)
+            {
+                pedido = new Pedido();
+                dbSets.Add(pedido);
+                applicationcontext.SaveChanges();
+            }
+            return pedido;
         }
 
         private int? GetPedidoId()
